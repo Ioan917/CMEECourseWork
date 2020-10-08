ae44d3a6b41dd4f86107769356b0487dfff52cdd
Ioan917 <ioan.evans17@imperial.ac.uk>
2020-10-08 12:11:52 +0100

    Added Week1 file.

---
+++ Week1/Code/boilerplate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## REVIEW what the below are for
+# Author: Ioan ie917@ic.ac.uk
+# Script: boilerplate.sh
+# Desc:simple boilerplate for shell scripts
+# Arguments: none
+# Date Oct 2020
+
+echo -e "\nThis is a shell script! \n" # find what does -e do
+
+#exit
