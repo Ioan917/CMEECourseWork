a49dd1c1cce7321a8b67316fdd3cb0117c17517a
Ioan917 <ie917@ic.ac.uk>
2020-11-22 23:26:02 +0000

    Trying .gitignore again

--- Week7/Sandbox/Code/NeedForSpeed.py
+++
@@ -1,5 +0,0 @@
-#### Numerical computing in python
-### Profiling
-# to find out what is slowing your code
-# locate the section of the code where speed bottlenecks exist
-# %run -p your_function_name
